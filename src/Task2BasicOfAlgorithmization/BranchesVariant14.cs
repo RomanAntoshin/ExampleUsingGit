4d956fd1ca889952aea11eaa5319e00a7e1e082e
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-02 13:07:21 +0300

    Make task about branches

---
+++ src/Task2BasicOfAlgorithmization/BranchesVariant14.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2BasicOfAlgorithmization
+{
+    public class BranchesVariant14
+    {
+        private double[] sides;
+        public BranchesVariant14(double[] sides)
+        {
+            this.sides = sides;
+        }
+        public bool ExistCheck()
+        {
+            Array.Sort(sides);
+            if (sides.Take(sides.Length - 1).Sum() <= sides[sides.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool TypeCheck()
+        {
+            double partSumSquare = 0;
+            for(int i=0; i<sides.Length-1; i++)
+            {
+                partSumSquare += Math.Pow(sides[i], 2);
+            }
+            if(Math.Pow(sides[sides.Length-1], 2)<partSumSquare)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Task2BasicOfAlgorithmization/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2BasicOfAlgorithmization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] sides = new double[] {6, 8, 10};
+            var branches = new BranchesVariant14(sides);
+            if(branches.ExistCheck())
+            {
+                Console.WriteLine("Exist");
+                if(branches.TypeCheck())
+                {
+                    Console.WriteLine("Sharp - angled");
+                }
+                else
+                {
+                    Console.WriteLine("Not sharp - angled");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not exist");
+            }
+
+        }
+    }
+}
