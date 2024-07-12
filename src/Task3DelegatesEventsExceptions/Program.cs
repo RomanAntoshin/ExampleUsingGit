53f0e584a9ff573a02ab4b2ee5a81fa7ca52f00f
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-12 12:06:05 +0300

    create project

---
+++ src/Task3DelegatesEventsExceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task3DelegatesEventsExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
