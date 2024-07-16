4c55a5712f4d1cef455d0b7232a5fe2abdd42474
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-16 13:29:30 +0300

    Remove method GetValue from third task

--- src/Task3DelegatesEventsExceptions/SquareTrinomial.cs
+++ src/Task3DelegatesEventsExceptions/SquareTrinomial.cs
@@ -3,10 +3,6 @@
     class SquareTrinomial
     {
         public delegate double Del(double x);
-        public static double GetValue(double a, double b, double c, double x)
-        {
-            return a * x * x + b * x + c;
-        }
         public static Del GetMethod(double a, double b, double c) => (double x) => a * x * x + b * x + c;
     }
 }
