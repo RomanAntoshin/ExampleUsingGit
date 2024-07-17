5e3e3aa942533535f0fbdc77a4319ceee02f0c7b
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-07-17 16:31:21 +0300

    Make test version first point

---
+++ Task4OOP/Consultant.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Task4OOP
+{
+    class Consultant
+    {
+        private IList<Client> clients;
+        public int Length { get { return clients.Count; } }
+        public Consultant(Client client)
+        {
+            this.clients = new Client[] { client };
+        }
+        public Consultant(IList<Client> clients) => this.clients = clients;
+        public string View(int ind)
+        {
+            return clients[ind].Surname + "\n" + clients[ind].Name + "\n" + clients[ind].Patronymic + "\n" + clients[ind].PhoneNumber + "\n" + "****" + "\n" + "******" + "\n";
+        }
+        public void ChangePhoneNumber(int ind, string phoneNumber)
+        {
+            if (phoneNumber != "")
+            {
+                clients[ind].PhoneNumber = phoneNumber;
+            }
+        }
+    }
+}
---
+++ Task4OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Task4OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> data;
+            using (StreamReader reader = new StreamReader("Database.txt"))
+            {
+                data = JsonSerializer.Deserialize<List<Client>>(reader.ReadToEnd());
+            }
+            Consultant consultant = new Consultant(data);
+            for (int i = 0; i < consultant.Length; i++)
+            {
+                Console.WriteLine(consultant.View(i));
+            }
+            consultant.ChangePhoneNumber(1, "8 922 994 77 88");
+            consultant.ChangePhoneNumber(0, "");
+            Console.WriteLine(consultant.View(0));
+            Console.WriteLine(consultant.View(1));
+        }
+    }
+}
